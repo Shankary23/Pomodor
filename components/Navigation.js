1877fec906fafa81b044efc7cef9df02d19c339e
Ryan Shankar <shankary@oregonstate.edu>
2024-10-29 22:48:47 -0700

    Initial commit

---
+++ components/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CgMusic } from "react-icons/cg";
+import { FiSettings, FiCommand } from "react-icons/fi";
+
+
+export default function Navigation() {
+  return (
+	<nav></nav>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+export default function Home() {
+	return(
+		<div className="bg-gray-900 min-h-screen font-inter">
+			<div className="max-w-2xl min-h-screen  mx-auto">
+				
+			</div>
+
+		</div>
+	
+	);
+}
+ 
