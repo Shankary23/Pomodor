3a287b5bf4148e709f631d5fb37690b62164c818
Ryan Shankar <shankary@oregonstate.edu>
2024-10-30 22:16:07 -0700

    Changed the about section will add some sort of col feature later"

--- components/About.js
+++ components/About.js
@@ -17,30 +17,7 @@ function About() {
 					that Cirillo used as a university student.
 				</p>
 			</div>
-			<div>
-				<h1 className="text-xl sm:text-2xl font-medium mt-5">
-					<span className="border-b-4 border-red-400">What</span> is Daily
-					Focus?
-				</h1>
-				<p className="mt-5 tracking-wide opacity-70 break-words text-lg break-words">
-					Daily focus is small clone project from{" "}
-					<a
-						href="https://pomofocus.io/"
-						target="_blank"
-						className=" underline"
-					>
-						https://pomofocus.io/
-					</a>
-					. It is open source and here is the code.{" "}
-					<a
-						href="https://github.com/Chensokheng/pomodoro"
-						target="_blank"
-						className=" underline"
-					>
-						https://github.com/Chensokheng/pomodoro
-					</a>
-				</p>
-			</div>
+			
 		</div>
 	);
 }
